9623b31df987b154f442a2948c2323021ad2452d
Cage <cage@elnino.tech>
2021-04-29 16:47:33 +0200

    add host room button

--- src/features/host/HostForm.tsx
+++ src/features/host/HostForm.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export const HostForm = () => {
+    const createRoom = () => {
+        // api create room
+    }
     return (
-        <div>
-            Host a room
-        </div>
+        <>
+            <div>
+                Host a room
+            </div>
+            <button onClick={createRoom}>host</button>
+        </>
     )
 }
