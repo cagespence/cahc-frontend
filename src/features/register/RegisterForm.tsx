43ca70452a5e6329a650738f98bfb3a3078c6ebe
Cage <cage@elnino.tech>
2021-04-29 14:01:13 +0200

    Change components folder to 'features'

---
+++ src/features/register/RegisterForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser, setToken } from '../../app/slices/userSlice'
+import { register } from '../../api/modules/auth';
+
+export const RegisterForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const registerUser = () => {
+        register({ username, password }).then(({ data: { username, token } }) => {
+            if (token) {
+                dispatch(setUser(username));
+                dispatch(setToken(token));
+            }
+        }).catch(error => {
+            console.log(error.status);
+            console.log(error.message)
+        })
+    }
+    return (
+        <>
+            <span className="page-title">register a new account</span>
+            <div>
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="new-password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <button onClick={registerUser}>register</button>
+            </div>
+        </>
+    )
+}
