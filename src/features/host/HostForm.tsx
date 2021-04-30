5f2bd0daf658f8071dd77d604757a97b3e993877
Cage <cage@elnino.tech>
2021-04-30 14:35:18 +0200

    Add join room functionality

--- src/features/host/HostForm.tsx
+++ src/features/host/HostForm.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-
+import { selectUser } from '../../app/slices/userSlice'
+import { useSelector } from 'react-redux';
+import { createRoom } from '../../socketio/socket';
+import { selectInRoom, selectIsHost } from '../../app/slices/roomSlice';
 export const HostForm = () => {
-    const createRoom = () => {
-        // api create room
+    const username = useSelector(selectUser);
+    const inRoom = useSelector(selectInRoom);
+    const isHost = useSelector(selectIsHost);
+    const handleCreateRoom = () => {
+        createRoom(username);
     }
     return (
         <>
             <div>
                 Host a room
             </div>
-            <button onClick={createRoom}>host</button>
+            <div>
+                {`${inRoom}`} - {`${isHost}`}
+            </div>
+            <button onClick={handleCreateRoom}>host</button>
         </>
     )
 }
