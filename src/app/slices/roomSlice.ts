ed63e916218337fc2136e5ab975856bab4825fc7
Cage <cage@elnino.tech>
2021-04-30 14:34:55 +0200

    Add store for game rooms

---
+++ src/app/slices/roomSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface RoomState {
+    inRoom: string | undefined;
+    isHost: boolean;
+}
+
+const initialState: RoomState = {
+    inRoom: undefined,
+    isHost: false,
+}
+
+export const roomSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setInRoom: (state, action) => {
+            return { ...state, inRoom: action.payload };
+        },
+        setIsHost: (state, action) => {
+            return { ...state, isHost: action.payload };
+        }
+    }
+})
+
+export const { setInRoom, setIsHost } = roomSlice.actions;
+
+export const selectInRoom = (state: any) => state.room.inRoom;
+export const selectIsHost = (state: any) => state.room.isHost;
+
+export default roomSlice.reducer;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../app/slices/userSlice';
+import roomReducer from '../app/slices/roomSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        room: roomReducer,
     },
 });
