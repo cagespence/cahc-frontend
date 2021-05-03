6bffdc4f086a1a9afcd052d87d56fa9dae49f018
Cage <cage@elnino.tech>
2021-05-03 13:57:36 +0300

    Add game lobby

--- src/features/lobby/Lobby.tsx
+++ src/features/lobby/Lobby.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect, useSelector } from 'react-redux';
 import { selectInRoom, selectMembers } from '../../app/slices/roomSlice';
+import './lobby.scss';
 
 export const Lobby = () => {
     const inRoom: string = useSelector(selectInRoom);
@@ -8,13 +9,20 @@ export const Lobby = () => {
     const Memberlist = () => {
         return (
             <>
-                {members.map(member => <div key={member}>{{ member }}</div>)}
+                <div className="lobby--room-players">
+                    players
+                </div>
+                {members.map(member => <div key={member}>{`${member}`}</div>)}
             </>
         )
     }
 
     return (
         <div>
+            <div>
+                in a game
+            </div>
+            room code: <span className="lobby--room-code">{`${inRoom}`}</span>
             <Memberlist />
         </div>
     )
